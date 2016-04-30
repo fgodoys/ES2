48371aee07a75209563b4f4992287c4b196c0800
Felipe Godoy <felipesilva349@gmail.com>
2016-04-30 15:25:23 -0300

    Change rating.c
    function

--- rating.c
+++ rating.c
@@ -4,20 +4,37 @@
 int main()
 {
 	float rounds, kills, deaths; 
-	float killrating, survivalrating, roundswithmultiplekillsrating, rating;
+	float rating;
 	float averageKPR = 0.679, averageSPR = 0.317, averageRMK = 1.277;
 	int k[5];// 1k, 2k, 3k, 4k, 5k (por round)
 	
-	killrating = (kills/rounds)/averageKPR;
-	survivalrating = ((rounds-deaths)/rounds)/averageSPR;
-	roundswithmultiplekillsrating = ((k[0]+(4*k[1])+(9*k[2])+(16*k[3])+(25*k[4]))/rounds)/averageRMK;
 	
 	
+	do
+	{
+		
+		rating = (killrating(kills, rounds, averageKPR) + 0.7*survivalrating(rounds, deaths, averageSPR) + roundswithmultiplekillsrating(k[5], rounds, averageRMK))/2.7;
+		printf("%0.2f\n\n", rating);
+	}while(rounds!=0)
 	
 	
-	rating = (killrating + 0.7*survivalrating + roundswithmultiplekillsrating)/2.7;
-	
-	printf("%0.2f\n\n",rating);
 	
 	
 }
+
+float killrating (float k, float r, float aKPR)
+{
+	return ((k/r)/aKPR);
+}
+
+float survivalrating (float r, float d, float aSPR)
+{
+	return (((r-d)/r)/aSPR);
+}
+
+float roundswithmultiplekillsrating (int k[5], float r, float aRMK)
+{
+	return (((k[0]+(4*k[1])+(9*k[2])+(16*k[3])+(25*k[4]))/r)/aRMK);
+}
+
+
