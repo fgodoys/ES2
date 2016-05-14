82ee783b0ac38240142dd8cc4eeaffc4e80761de
Felipe Godoy <felipesilva349@gmail.com>
2016-05-13 22:54:47 -0300

    Change rating.c
    Verificações

--- rating.c
+++ rating.c
@@ -3,7 +3,6 @@
 float killrating (float k, float r, float aKPR);
 float survivalrating (float r, float d, float aSPR);
 float roundswithmultiplekillsrating (float k[6], float r, float aRMK);
-void getdados();
 
 int main()
 {
@@ -18,29 +17,45 @@ int main()
 		printf("Total Rounds: ");
 		scanf("%f", &rounds);
 		if(rounds<=0)
-	        return;
+	        	return;
 	
 		printf("Total Kills: ");
 		scanf("%f", &kills);
 	
 		printf("Total Deaths: ");
 		scanf("%f", &deaths);
+		if(kills*5>rounds*5 || kills<0)
+		{
+			printf("Numero de kills invalido\n");
+			return;
+		}
+		elseif( || deaths <0 || deaths > rounds)
+		{
+			printf("Numero de deaths invalido\n");
+			return;
+		}
+		
 	
 		for(i=1; i<=5;i++)
 		{
 			printf("Total %iK: ",i);
 			scanf("%f", &k[i]);
+			j+=k[i];
+		}
+		if(j!=kills)
+		{
+			printf("Numero de kills ou multiplas kills nao sao iguais\n");
+			return;
 		}
 	
 		rating = (killrating(kills, rounds, averageKPR) + 0.7*survivalrating(rounds, deaths, averageSPR) + roundswithmultiplekillsrating(k, rounds, averageRMK))/2.7;
-		printf("KRRRRRRRR %f", killrating(kills, rounds, averageKPR));
-		printf("SR %f", survivalrating(rounds, deaths, averageSPR));
-		printf ("BBBB %f", roundswithmultiplekillsrating(k, rounds, averageRMK));
-		for(i=1; i<=5;i++)
-		{
-			printf("Total %fK: ",k[i]);
-		}
-		if(rounds!=0)
+		
+		//for(i=1; i<=5;i++)
+			//printf("Total %fK: ",k[i]);
+		
+		printf("Kill Rating: %f\n", killrating(kills, rounds, averageKPR));
+		printf("Survival Rating:  %f\n", survivalrating(rounds, deaths, averageSPR));
+		printf ("Rounds with multiple kills: %f\n", roundswithmultiplekillsrating(k, rounds, averageRMK));
 		printf("Rating: %0.2f\n\n", rating);
 	}while(rounds>0);
 
